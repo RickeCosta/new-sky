0101276e5d6ba3e6c3cd8cebc23313425628d425
Henrique <henriqueoliveiracosta@outlook.com>
2023-11-23 19:38:41 -0300

    teste

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const ham = document.querySelector('.menu-hamburguer2');
+const nav = document.querySelector('.card-avancar');
+
+
+ham.addEventListener('click',()=>{
+  nav.classList.toggle("active")
+})
+
+
+
